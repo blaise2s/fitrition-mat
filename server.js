832314687366c2b48293d85448e827862d542017
blaise2s <blaise.schaeffer@gmail.com>
2020-03-26 10:48:08 -0400

    feat(DEPLOY): Adds server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/fitrition'));
+
+// Send all requests to index.html
+app.get('/*', function (_req, res) {
+  console.log(req);
+  res.sendFile(path.join(__dirname + '/dist/fitrition/index.html'));
+});
+
+// default Heroku PORT
+const port = +process.env.PORT || 4200;
+app.listen(port);
+console.log(`🚀 server started on port ${port}.`);
